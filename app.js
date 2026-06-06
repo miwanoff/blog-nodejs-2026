8f341ddf722efa4e5de2174480f270d8bad3ab03
miwanoff <marina.iva.noffa@gmail.com>
2026-06-06 12:40:52 +0300

    Add app.js

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+
+
+// Підключення шаблонізатора EJS
+app.set("view engine", "ejs");
+
+
+// Налаштування доступу до локальних файлів Bootstrap 5
+app.use(
+  "/bootstrap/css",
+  express.static(__dirname + "/node_modules/bootstrap/dist/css"),
+);
+app.use(
+  "/bootstrap/js",
+  express.static(__dirname + "/node_modules/bootstrap/dist/js"),
+);
+
+
+// Роут головної сторінки
+app.get("/", function (req, res) {
+  res.render("index", { activePage: "home" });
+});
+
+
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
